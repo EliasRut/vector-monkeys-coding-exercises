13e05d0b33623e9d2ffeb1bb7a3005e60ab9e9b1
Elias Rut <eli4srut@gmail.com>
2020-07-15 21:47:27 +0200

    Removed solutions, added README.md

---
+++ README.md
@@ -0,0 +1,37 @@
+# Coding Session 2, Group 2
+This session is all about solving some coding exercises in JavaScript.
+
+## Getting started
+Before you start solving the exercises, you will need to pull them to your local computer.
+To do that, we will use git clone. This will generate a new directory inside the one you are currently in.
+
+Navigate to where you want to generate this repository by using your console and the cd command.
+
+Once you are where you want to be, run
+```
+git clone https://github.com/EliasRut/cs-group2-session2.git
+```
+
+## Installing dependencies
+The next thing you will want to do is install the nodejs dependencies. These are only required for
+running the tests.
+
+To do that, go into your newly generated directory by using cd, and then run
+```
+npm install
+```
+
+## Switching to your branch
+Since everybody should be working on their own solution, let's all switch to a separate git branch.
+
+You can do this by executing
+```
+git checkout -b YOUR_USERNAME
+```
+
+## Running the tests
+You can now start implementing your solutions. If you want to see whether they are correct, just run
+the tests that are included in exercises.test.js. To do so, just run
+```
+npm run test
+```
--- exercises.js
+++ exercises.js
@@ -1,65 +1,53 @@
-// You can execute the tests by running "npm run test" in the repositories root directory.
+// To get started, run "npm install" in the console in the repositories root directory.
+// You can then execute the tests by running "npm run test" in the repositories root directory.
 
 const exercise1 = () => {
 	// TODO: Return 42.
-	return 42;
 };
 
 const exercise2 = () => {
 	// TODO: Return the name of the author of Hitchhikers guide to the galaxy.
-	return "Douglas Adams";
 };
 
 const exercise3 = (firstNumber, secondNumber) => {
 	// TODO: Return the sum of firstNumber and secondNumber
-	return firstNumber + secondNumber;
 };
 
 const exercise4 = (firstNumber, secondNumber) => {
 	// TODO: Return the higher number of firstNumber or secondNumber
-	return Math.max(firstNumber, secondNumber);
 }
 
 const exercise5 = (number) => {
 	// TODO: Return whether or not the passed number is even
-	return number % 2 === 0;
 }
 
 const exercise6 = (author) => {
 	// TODO: Return whether or not the passed name is the author of Hitchhiker guide to the galaxy
-	return author === "Douglas Adams";
 }
 
 const exercise7 = (numbers) => {
 	// TODO: Return the sum of all numbers in the passed list
-	return numbers.reduce((sum, number) => sum + number, 0);
 }
 
 const exercise8 = (numbers) => {
 	// TODO: Return true if all numbers in the list are even
-	return numbers.every((number) => number % 2 === 0);
 }
 
 const exercise9 = (authors) => {
 	// TODO: Return whether or not the author of Hitchhiker guide to the galaxy is in the list of authors
-	return !!authors.find((author) => author === "Douglas Adams");
 }
 
 const exercise10 = (people) => {
 	// TODO: Return the name of all people who's favouriteColor is blue
-	return people.filter((person) => person.favouriteColor === 'blue').map((person) => person.name);
 }
 
 const exercise11 = (authors) => {
 	// TODO: Return the average age of all passed authors. Return null if no authors are passed.
-	if (!authors || !authors.length) return null;
-	return authors.reduce((sum, cur) => sum + cur.age, 0) / authors.length;
 }
 
 const exercise12 = (authors, getColorForName) => {
 	// TODO: Return all authors expanded with their favouriteColor, by using the getColorForName
 	// The getColorForName takes the authors name as parameter
-	return authors.map((author) => ({...author, favouriteColor: getColorForName(author.name)}));
 }
 
 module.exports = {
