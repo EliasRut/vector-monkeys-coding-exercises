53c896f052bdc9b05589192df8c818caead5c1ac
Elias Rut <eli4srut@gmail.com>
2020-07-16 17:39:01 +0200

    Fixed some syntax mistakes. Implemented a mistake to generate a merge conflict.

--- exercises.js
+++ exercises.js
@@ -3,6 +3,7 @@
 
 const exercise1 = () => {
 	// TODO: Return 42.
+	return 40;
 };
 
 const exercise2 = () => {
@@ -15,40 +16,40 @@ const exercise3 = (firstNumber, secondNumber) => {
 
 const exercise4 = (firstNumber, secondNumber) => {
 	// TODO: Return the higher number of firstNumber or secondNumber
-}
+};
 
 const exercise5 = (number) => {
 	// TODO: Return whether or not the passed number is even
-}
+};
 
 const exercise6 = (author) => {
 	// TODO: Return whether or not the passed name is the author of Hitchhiker guide to the galaxy
-}
+};
 
 const exercise7 = (numbers) => {
 	// TODO: Return the sum of all numbers in the passed list
-}
+};
 
 const exercise8 = (numbers) => {
 	// TODO: Return true if all numbers in the list are even
-}
+};
 
 const exercise9 = (authors) => {
 	// TODO: Return whether or not the author of Hitchhiker guide to the galaxy is in the list of authors
-}
+};
 
 const exercise10 = (people) => {
 	// TODO: Return the name of all people who's favouriteColor is blue
-}
+};
 
 const exercise11 = (authors) => {
 	// TODO: Return the average age of all passed authors. Return null if no authors are passed.
-}
+};
 
 const exercise12 = (authors, getColorForName) => {
 	// TODO: Return all authors expanded with their favouriteColor, by using the getColorForName
 	// The getColorForName takes the authors name as parameter
-}
+};
 
 module.exports = {
 	exercise1,
@@ -63,4 +64,4 @@ module.exports = {
 	exercise10,
 	exercise11,
 	exercise12,
-}
+};
--- exercises.test.js
+++ exercises.test.js
@@ -20,144 +20,144 @@ describe('Exercise 1', () => {
 	});
 });
 
-describe('Exercise 2', () => {
-	test('Returns "Douglas Adams"', () => {
-		expect(exercise2()).toBe("Douglas Adams");
-	});
-});
+// describe('Exercise 2', () => {
+// 	test('Returns "Douglas Adams"', () => {
+// 		expect(exercise2()).toBe("Douglas Adams");
+// 	});
+// });
 
-describe('Exercise 3', () => {
-	test('Returns 28 for 10, 18', () => {
-		expect(exercise3(10, 18)).toBe(28);
-	});
+// describe('Exercise 3', () => {
+// 	test('Returns 28 for 10, 18', () => {
+// 		expect(exercise3(10, 18)).toBe(28);
+// 	});
 
-	test('Returns -4 for 10, -14', () => {
-		expect(exercise3(10, -14)).toBe(-4);
-	});
-});
+// 	test('Returns -4 for 10, -14', () => {
+// 		expect(exercise3(10, -14)).toBe(-4);
+// 	});
+// });
 
-describe('Exercise 4', () => {
-	test('Returns 18 for 10, 18', () => {
-		expect(exercise4(10, 18)).toBe(18);
-	});
+// describe('Exercise 4', () => {
+// 	test('Returns 18 for 10, 18', () => {
+// 		expect(exercise4(10, 18)).toBe(18);
+// 	});
 
-	test('Returns -4 for -10, -4', () => {
-		expect(exercise4(-10, -4)).toBe(-4);
-	});
-});
+// 	test('Returns -4 for -10, -4', () => {
+// 		expect(exercise4(-10, -4)).toBe(-4);
+// 	});
+// });
 
-describe('Exercise 5', () => {
-	test('Returns true for 16', () => {
-		expect(exercise5(16)).toBe(true);
-	});
+// describe('Exercise 5', () => {
+// 	test('Returns true for 16', () => {
+// 		expect(exercise5(16)).toBe(true);
+// 	});
 
-	test('Returns true for -4', () => {
-		expect(exercise5(-4)).toBe(true);
-	});
+// 	test('Returns true for -4', () => {
+// 		expect(exercise5(-4)).toBe(true);
+// 	});
 
-	test('Returns false for 11', () => {
-		expect(exercise5(11)).toBe(false);
-	});
-});
+// 	test('Returns false for 11', () => {
+// 		expect(exercise5(11)).toBe(false);
+// 	});
+// });
 
-describe('Exercise 6', () => {
-	test('Returns true for "Douglas Adams"', () => {
-		expect(exercise6("Douglas Adams")).toBe(true);
-	});
+// describe('Exercise 6', () => {
+// 	test('Returns true for "Douglas Adams"', () => {
+// 		expect(exercise6("Douglas Adams")).toBe(true);
+// 	});
 
-	test('Returns false for "J.R.R. Tolkin"', () => {
-		expect(exercise6("J.R.R. Tolkin")).toBe(false);
-	});
-});
+// 	test('Returns false for "J.R.R. Tolkin"', () => {
+// 		expect(exercise6("J.R.R. Tolkin")).toBe(false);
+// 	});
+// });
 
-describe('Exercise 7', () => {
-	test('Returns 20 for [2, 4, 6, 8]', () => {
-		expect(exercise7([2, 4, 6, 8])).toBe(20);
-	});
+// describe('Exercise 7', () => {
+// 	test('Returns 20 for [2, 4, 6, 8]', () => {
+// 		expect(exercise7([2, 4, 6, 8])).toBe(20);
+// 	});
 
-	test('Returns 0 for -2, -1, 0, 1, 2', () => {
-		expect(exercise7([-2, -1, 0, 1, 2])).toBe(0);
-	});
-});
+// 	test('Returns 0 for -2, -1, 0, 1, 2', () => {
+// 		expect(exercise7([-2, -1, 0, 1, 2])).toBe(0);
+// 	});
+// });
 
-describe('Exercise 8', () => {
-	test('Returns true for [2, 4, 6, 8]', () => {
-		expect(exercise8([2, 4, 6, 8])).toBe(true);
-	});
+// describe('Exercise 8', () => {
+// 	test('Returns true for [2, 4, 6, 8]', () => {
+// 		expect(exercise8([2, 4, 6, 8])).toBe(true);
+// 	});
 
-	test('Returns false for -2, -1, 0, 1, 2', () => {
-		expect(exercise8([-2, -1, 0, 1, 2])).toBe(false);
-	});
-});
+// 	test('Returns false for -2, -1, 0, 1, 2', () => {
+// 		expect(exercise8([-2, -1, 0, 1, 2])).toBe(false);
+// 	});
+// });
 
-describe('Exercise 9', () => {
-	test('Returns true for ["Douglas Adams", "Brandon Sanderson"]', () => {
-		expect(exercise9(["Douglas Adams", "Brandon Sanderson"])).toBe(true);
-	});
+// describe('Exercise 9', () => {
+// 	test('Returns true for ["Douglas Adams", "Brandon Sanderson"]', () => {
+// 		expect(exercise9(["Douglas Adams", "Brandon Sanderson"])).toBe(true);
+// 	});
 
-	test('Returns false for []', () => {
-		expect(exercise9([])).toBe(false);
-	});
+// 	test('Returns false for []', () => {
+// 		expect(exercise9([])).toBe(false);
+// 	});
 
-	test('Returns false for ["Brandon Sanderson", "Wolfgang Hohlbein"]', () => {
-		expect(exercise9(["Brandon Sanderson", "Wolfgang Hohlbein"])).toBe(false);
-	});
-});
+// 	test('Returns false for ["Brandon Sanderson", "Wolfgang Hohlbein"]', () => {
+// 		expect(exercise9(["Brandon Sanderson", "Wolfgang Hohlbein"])).toBe(false);
+// 	});
+// });
 
-const authorTestSet = [
-	{
-		name: "Douglas Adams",
-		age: 42,
-		favouriteColor: "blue"
-	},
-	{
-		name: "Wolfgang Hohlbein",
-		age: 101,
-		favouriteColor: "red"
-	},
-	{
-		name: "Brandon Sanderson",
-		age: 55,
-		favouriteColor: "blue"
-	}
-];
+// const authorTestSet = [
+// 	{
+// 		name: "Douglas Adams",
+// 		age: 42,
+// 		favouriteColor: "blue"
+// 	},
+// 	{
+// 		name: "Wolfgang Hohlbein",
+// 		age: 101,
+// 		favouriteColor: "red"
+// 	},
+// 	{
+// 		name: "Brandon Sanderson",
+// 		age: 55,
+// 		favouriteColor: "blue"
+// 	}
+// ];
 
-describe('Exercise 10', () => {
-	test('Returns ["Douglas Adams", "Brandon Sanderson"] for the author test set', () => {
-		expect(exercise10(authorTestSet)).toStrictEqual(["Douglas Adams", "Brandon Sanderson"]);
-	});
+// describe('Exercise 10', () => {
+// 	test('Returns ["Douglas Adams", "Brandon Sanderson"] for the author test set', () => {
+// 		expect(exercise10(authorTestSet)).toStrictEqual(["Douglas Adams", "Brandon Sanderson"]);
+// 	});
 
-	test('Returns [] for an empty test set', () => {
-		expect(exercise10([])).toStrictEqual([]);
-	});
-});
+// 	test('Returns [] for an empty test set', () => {
+// 		expect(exercise10([])).toStrictEqual([]);
+// 	});
+// });
 
-describe('Exercise 11', () => {
-	test('Returns 66 for the author test set', () => {
-		expect(exercise11(authorTestSet)).toBe(66);
-	});
+// describe('Exercise 11', () => {
+// 	test('Returns 66 for the author test set', () => {
+// 		expect(exercise11(authorTestSet)).toBe(66);
+// 	});
 
-	test('Returns null for an empty test set', () => {
-		expect(exercise11([])).toBe(null);
-	});
+// 	test('Returns null for an empty test set', () => {
+// 		expect(exercise11([])).toBe(null);
+// 	});
 
-	test('Returns the single authors age if only one author is passed', () => {
-		expect(exercise11([authorTestSet[0]])).toBe(42);
-	});
-});
+// 	test('Returns the single authors age if only one author is passed', () => {
+// 		expect(exercise11([authorTestSet[0]])).toBe(42);
+// 	});
+// });
 
-const getColorForName = (name) => name.startsWith('D') || name.startsWith('B') ? "blue" : "red";
+// const getColorForName = (name) => name.startsWith('D') || name.startsWith('B') ? "blue" : "red";
 
-describe('Exercise 12', () => {
-	test('Returns correctly set colors for the author test set', () => {
-		const colorStrippedAuthors = authorTestSet.map((author) => ({
-			name: author.name,
-			age: author.age,
-		}));
-		expect(exercise12(colorStrippedAuthors, getColorForName)).toStrictEqual(authorTestSet);
-	});
+// describe('Exercise 12', () => {
+// 	test('Returns correctly set colors for the author test set', () => {
+// 		const colorStrippedAuthors = authorTestSet.map((author) => ({
+// 			name: author.name,
+// 			age: author.age,
+// 		}));
+// 		expect(exercise12(colorStrippedAuthors, getColorForName)).toStrictEqual(authorTestSet);
+// 	});
 
-	test('Returns [] for an empty test set', () => {
-		expect(exercise12([])).toStrictEqual([]);
-	});
-});
+// 	test('Returns [] for an empty test set', () => {
+// 		expect(exercise12([])).toStrictEqual([]);
+// 	});
+// });
